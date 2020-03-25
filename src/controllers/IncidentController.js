ed73b8c0c2a9f041458fb8c69f0c213ce9b1b78d
santosdanilo <dandesenvolvimento@gmail.com>
2020-03-25 14:28:21 -0300

    Adicionada contagem de elementos total

--- src/controllers/IncidentController.js
+++ src/controllers/IncidentController.js
@@ -3,10 +3,16 @@ const connection = require('../database/connection')
 module.exports = {
     async index(request, response) {
         const { page = 1 } = request.query
+
+        const [count] = await connection('incidents').count()
+
         const incidents = await connection('incidents')
             .limit(5)
             .offset((page - 1) * 5)
             .select('*')
+
+        response.header('X-Total-Count', count['count(*)'])
+
         return response.json(incidents)
     },
     async create(request, response) {
