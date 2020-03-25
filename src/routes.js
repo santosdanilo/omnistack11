edfd13c7ec5406734b9e2b2739cb9bf9c030085e
santosdanilo <dandesenvolvimento@gmail.com>
2020-03-25 13:45:31 -0300

    Adicionada rota de cadastro de ong

--- src/routes.js
+++ src/routes.js
@@ -1,23 +1,24 @@
 const express = require('express');
+const crypto = require('crypto');
+const connection = require('./database/connection')
 
-const routes = express.Router()
+const routes = express.Router();
 
-routes.get('/users/:id', (request, response) => {
-    const params = request.params
+routes.post('/ongs', async (request, response) => {
+    const { name, email, whatsapp, city, uf } = request.body
 
-    return response.json({
-        event: 'Semana Omnistack 11.0',
-        student: 'Danilo Santos'
+    const id = crypto.randomBytes(4).toString('HEX')
+
+    await connection('ongs').insert({
+        id,
+        name,
+        email,
+        whatsapp,
+        city,
+        uf
     })
-})
 
-routes.post('/users', (request, response) => {
-    const body = request.body
-
-    return response.json({
-        event: 'Semana Omnistack 11.0',
-        student: 'Danilo Santos'
-    })
+    return response.json({ id })
 })
 
 module.exports = routes
