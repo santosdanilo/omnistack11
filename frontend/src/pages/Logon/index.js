49783eefa4f2acda6ac85ba493ad0f057c14bfb0
santosdanilo <dandesenvolvimento@gmail.com>
2020-03-26 09:29:50 -0300

    Adicionado router e rota do register

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
 
 import './styles.css'
@@ -14,7 +15,7 @@ export default function Logon() {
                     <h1>Faça seu logon</h1>
                     <input placeholder="Sua ID" />
                     <button className="button" type="submit">Entrar</button>
-                    <a href="/register"><FiLogIn size={16} color="#E02041" />Não tenho cadastro</a>
+                    <Link to="/signup"><FiLogIn size={16} color="#E02041" />Não tenho cadastro</Link>
                 </form>
             </section>
             <img src={heroesImage} alt="Heroes" />
