359b397f89d6dc24d8e61d914600de486701b30a
santosdanilo <dandesenvolvimento@gmail.com>
2020-03-26 23:40:16 -0300

    Adicionada pagina de novo caso

--- frontend/src/pages/NewIncident/index.js
+++ frontend/src/pages/NewIncident/index.js
@@ -1,6 +1,26 @@
 import React from 'react';
-import './styles.css'
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
+import './styles.css';
+import logoImage from '../../assets/logo.svg'
 
-export default function NewIncident() {
-    return (<div>a</div>)
+export default function NewIndicent() {
+    return (
+        <div className="new-incident-container">
+            <div className="content">
+                <section>
+                    <img src={logoImage} alt="Be The Hero" />
+                    <h1>Cadastrar novo caso</h1>
+                    <p>Descreva o caso detalhadamente para encontrar um herói para resolver isso.</p>
+                    <Link to="/profile" className="back-link"><FiArrowLeft size={16} color="#E02041" />Voltar para home</Link>
+                </section>
+                <form>
+                    <input placeholder="Titulo do caso" />
+                    <textarea placeholder="Descrição" />
+                    <input placeholder="Valor em reai" />
+                    <button className="button" type="submit">Cadastrar</button>
+                </form>
+            </div>
+        </div>
+    )
 }
