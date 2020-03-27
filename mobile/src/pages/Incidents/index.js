6816167c9ad63b15e89305ca4e1e7003e95e2ded
santosdanilo <dandesenvolvimento@gmail.com>
2020-03-27 14:34:19 -0300

    Adicionado cerregamento da paginacao

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -13,24 +13,44 @@ export default function Incidents() {
 
     const [incidents, setIncidents] = useState([])
     const [total, setTotal] = useState(0)
+    const [page, setPage] = useState(1)
+    const [loading, setLoading] = useState(false)
+
+    useEffect(() => {
+        loadIncidents()
+    }, [])
 
     function navigatoToDetail(incident) {
         navigation.navigate('Detail', { incident })
     }
-    async function loadIncidents() {
-        const response = await api.get('incidents');
 
-        setIncidents(response.data)
+    async function loadIncidents() {
+        if (loading) {
+            return;
+        }
+
+        if (total > 0 && incidents.length === total) {
+            return;
+        }
+
+        setLoading(true)
+
+        const response = await api.get('incidents', {
+            params: {
+                page
+            }
+        });
+
+
+        setIncidents([...incidents, ...response.data])
         setTotal(response.headers['x-total-count'])
+        setPage(page + 1)
+        setLoading(false)
     }
 
     function formatCurrency(value) {
         return Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(value)
     }
-
-    useEffect(() => {
-        loadIncidents()
-    }, [])
 
     return (
         <View style={styles.container}>
@@ -47,7 +67,9 @@ export default function Incidents() {
                 data={incidents}
                 style={styles.incidentList}
                 keyExtractor={incident => String(incident.id)}
-                showsVerticalScrollIndicator={false}
+                //showsVerticalScrollIndicator={false}
+                onEndReached={loadIncidents}
+                onEndReachedThreshold={0.2}
                 renderItem={({ item: incident }) => (
                     <View style={styles.incident}>
                         <Text style={styles.incidentProperty}>ONG:</Text>
